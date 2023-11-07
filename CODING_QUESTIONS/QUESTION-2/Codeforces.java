ac538abf4427ba26b1a56d3799efbecfaaa09b39
Himang Garg <130616649+himang4321@users.noreply.github.com>
2023-11-07 08:23:08 +0530

    Update Codeforces.java

--- CODING_QUESTIONS/QUESTION-2/Codeforces.java
+++ CODING_QUESTIONS/QUESTION-2/Codeforces.java
@@ -1,3 +1,21 @@
+// Hilbert's Hotel is a very unusual hotel since the number of rooms is infinite! 
+// In fact, there is exactly one room for every integer, including zero and negative integers.
+// Even stranger, the hotel is currently at full capacity, meaning there is exactly one guest in every room.
+// The hotel's manager, David Hilbert himself, decides he wants to shuffle the guests around because he thinks this will create a vacancy (a room without a guest).
+//
+// For any integer k and positive integer n, let kmodn denote the remainder when k is divided by n. 
+// More formally, r=kmodn is the smallest non-negative integer such that k−r is divisible by n.
+// It always holds that 0≤kmodn≤n−1.
+// For example, 100mod12=4 and (−1337)mod3=1.
+//
+// Then the shuffling works as follows. There is an array of n integers a0,a1,…,an−1.
+// Then for each integer k, the guest in room k is moved to room number k+akmodn.
+//
+// After this shuffling process, determine if there is still exactly one guest assigned to each room.
+// That is, there are no vacancies or rooms with multiple guests.
+
+//link to the problem : https://codeforces.com/contest/1344/problem/A
+
 import java.math.BigInteger;
 import java.util.*;
 
