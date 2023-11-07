b5e05fd1a18f27b18d24a7e2d2b9d3723d2e9887
Himang Garg <130616649+himang4321@users.noreply.github.com>
2023-11-07 06:51:06 +0530

    Done using BigInteger

--- CODING_QUESTIONS/QUESTION-2/Codeforces.java
+++ CODING_QUESTIONS/QUESTION-2/Codeforces.java
@@ -1,22 +1,23 @@
+import java.math.BigInteger;
 import java.util.*;
- 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
         for(int j=0;j<t;j++) {
             int n = sc.nextInt();
-            ArrayList<Integer> l = new ArrayList<>();
+            ArrayList<BigInteger> l = new ArrayList<>();
             for (int i = 0; i < n; i++) {
-                l.add(sc.nextInt());
+                l.add(sc.nextBigInteger());
             }
             for (int i = 0; i < n; i++) {
-                l.set(i, l.get(i) + i);
+                l.set(i, l.get(i).add(BigInteger.valueOf(i)));
             }
             for (int i = 0; i < n; i++) {
-                l.set(i, l.get(i) % n);
+                l.set(i, l.get(i).mod(BigInteger.valueOf(n)));
             }
-            Set<Integer> set = new HashSet<Integer>();
+            Set<BigInteger> set = new HashSet<BigInteger>();
             for (int i = 0; i < n; i++) {
                 set.add(l.get(i));
             }
